50b9a6de6d50e6b5a72da14f0fcb20a89330e9dd
ValeraPL <mafarawa5@gmail.com>
2019-12-16 09:07:30 +0200

    Сделать функцию которая узнает тип данных на потоке

--- Main4/main.cpp
+++ Main4/main.cpp
@@ -12,11 +12,11 @@ int main(int argc, char *argv[]) {
 	while(start == true) {
 		Bike bike;
 
-		cout << "Введите размер колеса: ";
+		cout << "Enter wheels inch: ";
 		cin >> inch;
 		bike.setWheelsInch(inch);
 
-		cout << "Введите цвет: ";
+		cout << "Enter color: ";
 		cin >> color;
 		bike.setColor(color);
 
@@ -24,8 +24,10 @@ int main(int argc, char *argv[]) {
 		file << "color: " << bike.getColor() << " inch: " << bike.getWheelsInch() << "\n";
 		file.close();
 
-		cout << "Записано" << endl;
+		cout << "WRITE" << endl;
 	}
 
 	return 0;
 }
+
+// TODO Узнать тип введенных данных
